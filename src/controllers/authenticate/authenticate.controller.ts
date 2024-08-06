83b492ad0265f879c41ca1fd7aa26221cd8fd9bb
Thiago Leite <thiagofmleite@gmail.com>
2024-08-06 17:35:28 -0300

    Adding Authenticate Controller

--- src/controllers/authenticate/authenticate.controller.ts
+++ src/controllers/authenticate/authenticate.controller.ts
@@ -1,14 +1,51 @@
-import { Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UnauthorizedException,
+  UsePipes,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { compare } from 'bcryptjs';
+import { ZodValidationPipe } from 'src/pipes';
+import {
+  AuthenticateBodySchema,
+  authenticateBodySchema,
+} from './authenticate.schema';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Controller('/sessions')
 export class AuthenticateController {
-  constructor(private jwt: JwtService) {}
+  constructor(
+    private jwt: JwtService,
+    private prisma: PrismaService,
+  ) {}
 
   @Post()
-  async handle() {
-    const token = this.jwt.sign({ sub: 'user-id' });
+  @UsePipes(new ZodValidationPipe(authenticateBodySchema))
+  async handle(@Body() body: AuthenticateBodySchema) {
+    const { email, password } = body;
 
-    return token;
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const isPasswordValid = await compare(password, user.password);
+
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const accessToken = this.jwt.sign({ sub: user.id });
+
+    return {
+      acess_token: accessToken,
+    };
   }
 }
--- src/controllers/authenticate/authenticate.schema.ts
+++ src/controllers/authenticate/authenticate.schema.ts
@@ -1,3 +1,12 @@
-export type AuthenticateBodySchema = {
-  token: string;
+import { z } from 'zod';
+
+export const authenticateBodySchema = z.object({
+  email: z.string().email(),
+  password: z.string(),
+});
+
+export type AuthenticateBodySchema = z.infer<typeof authenticateBodySchema>;
+
+export type AuthenticateResponseSchema = {
+  acess_token: string;
 };
