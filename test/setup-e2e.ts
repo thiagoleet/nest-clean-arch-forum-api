1310ffb489e0a1f634d6705eea7f023cd2afd94b
Thiago Leite <thiagofmleite@gmail.com>
2024-08-19 19:43:42 -0300

    Preparing database schema for E2E tests

---
+++ test/setup-e2e.ts
@@ -0,0 +1,41 @@
+import 'dotenv/config';
+import { PrismaClient } from '@prisma/client';
+import { randomUUID } from 'node:crypto';
+import { execSync } from 'node:child_process';
+
+const prisma = new PrismaClient();
+const schemaId = randomUUID();
+
+function generateUniqueDatabaseURL(schemaId: string) {
+  if (!schemaId) {
+    throw new Error('schemaId is required');
+  }
+
+  if (!process.env.DATABASE_URL) {
+    throw new Error('DATABASE_URL is not set');
+  }
+
+  const url = new URL(process.env.DATABASE_URL);
+
+  url.searchParams.set('schema', schemaId);
+
+  return url.toString();
+}
+
+async function dropSchema(schema: string) {
+  await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${schema}" CASCADE`);
+
+  await prisma.$disconnect();
+}
+
+beforeAll(async () => {
+  const databaseURL = generateUniqueDatabaseURL(schemaId);
+
+  process.env.DATABASE_URL = databaseURL;
+
+  execSync('npx prisma migrate deploy');
+});
+
+afterAll(async () => {
+  await dropSchema(schemaId);
+});
