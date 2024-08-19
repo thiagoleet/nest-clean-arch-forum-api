b2880830436882afcc6d5769891d0f27c4c70099
Thiago Leite <thiagofmleite@gmail.com>
2024-08-19 19:54:00 -0300

    Added Create Account Controller E2E test

--- src/controllers/create-account/create-account.controller.e2e-spec.ts
+++ src/controllers/create-account/create-account.controller.e2e-spec.ts
@@ -1,3 +1,31 @@
-test('1 plus 1', () => {
-  expect(1 + 1).toBe(2);
+import request from 'supertest';
+import { INestApplication } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import { AppModule } from '@/app.module';
+
+describe('[E2E] CreateAccountController', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+    await app.init();
+  });
+
+  test(`/GET accounts`, async () => {
+    const response = await request(app.getHttpServer()).post('/accounts').send({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    expect(response.statusCode).toBe(201);
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
 });
