fc17f15850a058e5d8da8185910e3f6f65e2e25f
Thiago Leite <thiagofmleite@gmail.com>
2025-07-30 09:37:21 -0300

    feat(tests): refactor OnAnswerCreated and OnQuestionBestAnswerChoosen tests to improve structure and include student and attachment handling

--- src/domain/notification/application/subscribers/on-answer-created/on-answer-created.spec.ts
+++ src/domain/notification/application/subscribers/on-answer-created/on-answer-created.spec.ts
@@ -1,16 +1,22 @@
-import { vi, MockInstance } from "vitest";
-import { makeAnswer } from "test/factories/forum/make-answer";
-import { OnAnswerCreated } from "./on-answer-created";
-import { InMemoryAnswersRepository } from "test/repositories/forum/in-memory-answers.repository";
-import { InMemoryQuestionsRepository } from "test/repositories/forum/in-memory-questions.repository";
-import { SendNotificationUseCase } from "../../use-cases/send-notification";
-import { InMemoryNotificationsRepository } from "test/repositories/notification/in-memory-notifications.repository";
-import { makeQuestion } from "test/factories/forum/make-question";
-import { waitFor } from "test/utils/wait-for";
+import { vi, MockInstance } from 'vitest';
+import { makeAnswer } from 'test/factories/forum/make-answer';
+import { OnAnswerCreated } from './on-answer-created';
+import { InMemoryAnswersRepository } from 'test/repositories/forum/in-memory-answers.repository';
+import { InMemoryQuestionsRepository } from 'test/repositories/forum/in-memory-questions.repository';
+import { SendNotificationUseCase } from '../../use-cases/send-notification';
+import { InMemoryNotificationsRepository } from 'test/repositories/notification/in-memory-notifications.repository';
+import { makeQuestion } from 'test/factories/forum/make-question';
+import { waitFor } from 'test/utils/wait-for';
+import { InMemoryAttachmentssRepository } from 'test/repositories/forum/in-memory-attachments.repository';
+import { InMemoryQuestionAttachmentsRepository } from 'test/repositories/forum/in-memory-question-attachments.repository';
+import { InMemoryStudentsRepository } from 'test/repositories/forum/in-memory-students.repository';
 
-describe("OnAnswerCreated", () => {
+describe('OnAnswerCreated', () => {
   let answersRepository: InMemoryAnswersRepository;
   let questionsRepository: InMemoryQuestionsRepository;
+  let attachmentsRepository: InMemoryAttachmentssRepository;
+  let questionAttachmentsRepository: InMemoryQuestionAttachmentsRepository;
+  let studentsRepository: InMemoryStudentsRepository;
   let sendNotificationUseCase: SendNotificationUseCase;
   let notificationsRepository: InMemoryNotificationsRepository;
 
@@ -18,18 +24,25 @@ describe("OnAnswerCreated", () => {
 
   beforeEach(() => {
     answersRepository = new InMemoryAnswersRepository();
-    questionsRepository = new InMemoryQuestionsRepository();
+    attachmentsRepository = new InMemoryAttachmentssRepository();
+    questionAttachmentsRepository = new InMemoryQuestionAttachmentsRepository();
+    studentsRepository = new InMemoryStudentsRepository();
+    questionsRepository = new InMemoryQuestionsRepository(
+      questionAttachmentsRepository,
+      attachmentsRepository,
+      studentsRepository,
+    );
     notificationsRepository = new InMemoryNotificationsRepository();
     sendNotificationUseCase = new SendNotificationUseCase(
-      notificationsRepository
+      notificationsRepository,
     );
 
-    sendNotificationSpy = vi.spyOn(sendNotificationUseCase, "execute");
+    sendNotificationSpy = vi.spyOn(sendNotificationUseCase, 'execute');
 
     new OnAnswerCreated(questionsRepository, sendNotificationUseCase);
   });
 
-  it("should send a notification when an answer is created", async () => {
+  it('should send a notification when an answer is created', async () => {
     // Creating a question
     const question = makeQuestion();
     await questionsRepository.create(question);
--- src/domain/notification/application/subscribers/on-question-best-answer-choosen/on-question-best-answer-choosen.spec.ts
+++ src/domain/notification/application/subscribers/on-question-best-answer-choosen/on-question-best-answer-choosen.spec.ts
@@ -1,16 +1,22 @@
-import { InMemoryAnswersRepository } from "test/repositories/forum/in-memory-answers.repository";
-import { InMemoryQuestionsRepository } from "test/repositories/forum/in-memory-questions.repository";
-import { InMemoryNotificationsRepository } from "test/repositories/notification/in-memory-notifications.repository";
-import { SendNotificationUseCase } from "../../use-cases/send-notification";
-import { MockInstance } from "vitest";
-import { OnQuestionBestAnswerChoosen } from "./on-question-best-answer-choosen";
-import { makeQuestion } from "test/factories/forum/make-question";
-import { makeAnswer } from "test/factories/forum/make-answer";
-import { waitFor } from "test/utils/wait-for";
+import { InMemoryAnswersRepository } from 'test/repositories/forum/in-memory-answers.repository';
+import { InMemoryQuestionsRepository } from 'test/repositories/forum/in-memory-questions.repository';
+import { InMemoryNotificationsRepository } from 'test/repositories/notification/in-memory-notifications.repository';
+import { SendNotificationUseCase } from '../../use-cases/send-notification';
+import { MockInstance } from 'vitest';
+import { OnQuestionBestAnswerChoosen } from './on-question-best-answer-choosen';
+import { makeQuestion } from 'test/factories/forum/make-question';
+import { makeAnswer } from 'test/factories/forum/make-answer';
+import { waitFor } from 'test/utils/wait-for';
+import { InMemoryAttachmentssRepository } from 'test/repositories/forum/in-memory-attachments.repository';
+import { InMemoryQuestionAttachmentsRepository } from 'test/repositories/forum/in-memory-question-attachments.repository';
+import { InMemoryStudentsRepository } from 'test/repositories/forum/in-memory-students.repository';
 
-describe("OnQuestionBestAnswerChoosen", () => {
+describe('OnQuestionBestAnswerChoosen', () => {
   let answersRepository: InMemoryAnswersRepository;
   let questionsRepository: InMemoryQuestionsRepository;
+  let attachmentsRepository: InMemoryAttachmentssRepository;
+  let questionAttachmentsRepository: InMemoryQuestionAttachmentsRepository;
+  let studentsRepository: InMemoryStudentsRepository;
   let sendNotificationUseCase: SendNotificationUseCase;
   let notificationsRepository: InMemoryNotificationsRepository;
 
@@ -18,18 +24,25 @@ describe("OnQuestionBestAnswerChoosen", () => {
 
   beforeEach(() => {
     answersRepository = new InMemoryAnswersRepository();
-    questionsRepository = new InMemoryQuestionsRepository();
+    attachmentsRepository = new InMemoryAttachmentssRepository();
+    questionAttachmentsRepository = new InMemoryQuestionAttachmentsRepository();
+    studentsRepository = new InMemoryStudentsRepository();
+    questionsRepository = new InMemoryQuestionsRepository(
+      questionAttachmentsRepository,
+      attachmentsRepository,
+      studentsRepository,
+    );
     notificationsRepository = new InMemoryNotificationsRepository();
     sendNotificationUseCase = new SendNotificationUseCase(
-      notificationsRepository
+      notificationsRepository,
     );
 
-    sendNotificationSpy = vi.spyOn(sendNotificationUseCase, "execute");
+    sendNotificationSpy = vi.spyOn(sendNotificationUseCase, 'execute');
 
     new OnQuestionBestAnswerChoosen(answersRepository, sendNotificationUseCase);
   });
 
-  it("should send notification when question has best answer choosen", async () => {
+  it('should send notification when question has best answer choosen', async () => {
     // Creating a question
     const question = makeQuestion();
     await questionsRepository.create(question);
