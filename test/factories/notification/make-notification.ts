983e1d3e5d7af08666c6669a4051c9db23d91972
Thiago Leite <thiagofmleite@gmail.com>
2025-08-14 19:16:05 -0300

    refactor(notification): standardize import quotes and improve formatting in make-notification.ts

--- test/factories/notification/make-notification.ts
+++ test/factories/notification/make-notification.ts
@@ -1,23 +1,43 @@
-import { faker } from "@faker-js/faker";
-import { UniqueEntityID } from "@/core/entities";
+import { faker } from '@faker-js/faker';
+import { UniqueEntityID } from '@/core/entities';
 import {
   Notification,
   NotificationProps,
-} from "@/domain/notification/enterprise/entities";
+} from '@/domain/notification/enterprise/entities';
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '@/infra/database/prisma/prisma.service';
+import { PrismaNotificationMapper } from '@/infra/database/prisma/mappers/prisma-notification.mapper';
 
 export function makeNotification(
   overide: Partial<NotificationProps> = {},
-  id?: UniqueEntityID
+  id?: UniqueEntityID,
 ) {
   const question = Notification.create(
     {
-      recipientId: new UniqueEntityID("recipient-id"),
+      recipientId: new UniqueEntityID('recipient-id'),
       title: faker.lorem.sentence(),
       content: faker.lorem.text(),
       ...overide,
     },
-    id
+    id,
   );
 
   return question;
+}
+
+@Injectable()
+export class NotificationFactory {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async makePrismaNotification(
+    data: Partial<NotificationProps> = {},
+  ): Promise<Notification> {
+    const notification = makeNotification(data);
+
+    await this.prisma.notification.create({
+      data: PrismaNotificationMapper.toPersistence(notification),
+    });
+
+    return notification;
+  }
 }
