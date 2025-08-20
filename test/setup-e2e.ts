350c1acbf62bf410df189903d35b49ae60c30a78
Thiago Leite <thiagofmleite@gmail.com>
2025-08-20 18:56:48 -0300

    feat(test): integrate Redis for end-to-end testing setup

--- test/setup-e2e.ts
+++ test/setup-e2e.ts
@@ -4,13 +4,23 @@ import { PrismaClient } from '@prisma/client';
 import { randomUUID } from 'node:crypto';
 import { execSync } from 'node:child_process';
 import { DomainEvents } from '@/core/events';
+import { Redis } from 'ioredis';
+import { envSchema } from '@/infra/http/env/env';
 
 // Load environment variables from .env files
 config({ path: '.env', override: true });
 config({ path: '.env.test', override: true });
 
+const env = envSchema.parse(process.env);
+
 const prisma = new PrismaClient();
 const schemaId = randomUUID();
+
+const redis = new Redis({
+  host: env.REDIS_HOST,
+  port: env.REDIS_PORT,
+  db: env.REDIS_DB,
+});
 
 function generateUniqueDatabaseURL(schemaId: string) {
   if (!schemaId) {
@@ -40,6 +50,8 @@ beforeAll(async () => {
   process.env.DATABASE_URL = databaseURL;
 
   DomainEvents.shouldRun = false;
+
+  await redis.flushdb();
 
   execSync('npx prisma migrate deploy');
 });
